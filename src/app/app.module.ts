8050092cfff7e056626c4f9bf89e25321ff6d49c
HatemAbid <hatem.imp@gmail.com>
2021-05-22 22:43:24 +0100

    SOLVED ISSUE 10 - DESIGN HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContentComponent } from './shared/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    DashboardComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
